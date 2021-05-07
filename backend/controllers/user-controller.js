083830a0875cfee8cf44773619337f20e60ad5d8
Pius <jonzyp4reala@gmail.com>
2021-05-07 05:22:56 +0100

    user endpoint and controller set-up

--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -42,3 +42,36 @@ export const getUserProfile = asyncHandler(async (req, res) => {
     throw new Error('User not found')
   }
 })
+
+//@desc get REgister User
+//@route Get /api/users
+//@access private
+export const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body
+
+  const userExists = await User.findOne({ email })
+
+  if (userExists) {
+    res.status(400)
+    throw new Error('User already exist')
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  })
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid user data')
+  }
+})
