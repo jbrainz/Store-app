ff49671a197ac211eff1d000217415ef773b5ebd
jbrainz <piusunimke@gmail.com>
2022-05-28 16:05:25 +0100

    added cors

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ dotenv.config()
 connectDB()
 
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: '*',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 const app = express()
