3eb29ae1cd6d6d56a3a47aa2ef5f1c7d52a70439
jbrainz <piusunimke@gmail.com>
2022-07-10 20:27:23 +0100

    fix order list bug

--- frontend/src/screens/OrderListScreen.js
+++ frontend/src/screens/OrderListScreen.js
@@ -55,18 +55,18 @@ const OrderListScreen = ({ history }) => {
                 <tr key={order._id}>
                   <td>{order._id}</td>
                   <td>{order.user && order.user.name}</td>
-                  <td>{order.createdAt.substring(0, 10)}</td>
+                  <td>{order?.createdAt}</td>
                   <td>${order.totalPrice}</td>
                   <td>
                     {order.isPaid ? (
-                      order?.paidAt.substring(0, 10)
+                      order?.paidAt
                     ) : (
                       <i className="fas fa-times" style={{ color: "red" }}></i>
                     )}
                   </td>
                   <td>
                     {order.isDelivered ? (
-                      order?.deliveredAt.substring(0, 10)
+                      order?.deliveredAt
                     ) : (
                       <i className="fas fa-times" style={{ color: "red" }}></i>
                     )}
